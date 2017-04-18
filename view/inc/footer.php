342842899a4cf7a25a69537dbb30db197226d124
Krasimir <Krasimir>
2017-04-19 00:51:07 +0300

    fix conflict + users.sql

--- view/inc/footer.php
+++ view/inc/footer.php
@@ -1,11 +1,8 @@
 	<footer id="footer" role="contentinfo">
 		<div class="footer-body">
 			<div class="footer-logo text-center">
-<<<<<<< HEAD
 				<img src="/FinalProject/view/images/atlassian-logo-grey.svg" alt="">
-=======
-				<img src="images/atlassian-logo-grey.svg" alt="">
->>>>>>> f13b29fe09a13113ee222f120838d6b032c41d13
+
 			</div>
 		</div>
 	</footer>
@@ -16,10 +13,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
    	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.0/jquery.validate.min.js"></script>	   
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<<<<<<< HEAD
+
     <script src="/FinalProject/view/js/app.js"></script>
-=======
-    <script src="js/app.js"></script>
->>>>>>> f13b29fe09a13113ee222f120838d6b032c41d13
+
   </body>
 </html>
