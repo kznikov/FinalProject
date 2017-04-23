f1be26b304f0b4c7abacc8ed1dae07df504a271e
Panayot Angarev <pancho.angarev@gmail.com>
2017-04-23 16:23:59 +0300

    avatar change

--- view/welcome.php
+++ view/welcome.php
@@ -1,15 +1,16 @@
 <?php 
   $pageTitle="Welcome to JIRA";
   include "inc/header.php"; 
-  header('Cache-Control: no cache'); //no cache
+// header('Cache-Control: no cache'); //no cache
   session_cache_limiter('private_no_expire'); // works
-  //session_cache_limiter('public'); // works too
-  session_start();
+  session_cache_limiter('public'); // works too
+//  session_start();
   if(!isset($_SESSION['user'])){
-  	header('Location:../view/index.php');
+    header('Location:../view/index.php');
   }
   
   $userData = json_decode($_SESSION['user'], true);
+  $user_id = $userData['id'];
 ?>
 
 <body>
@@ -27,14 +28,14 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-			<li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $userData['lastname'].", ".$userData['firstname']." " ?><span class="caret"></span></a>
-			<ul class="dropdown-menu">
+      <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $userData['lastname'].", ".$userData['firstname']." " ?><span class="caret"></span></a>
+      <ul class="dropdown-menu">
 
-				<li><a href="/FinalProject/controller/LogoutController.php">Logout</a></li>
+        <li><a href="/FinalProject/controller/LogoutController.php">Logout</a></li>
 
-			</ul>
-			</li>
+      </ul>
+      </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -47,7 +48,7 @@
         <hr>
         <div class="avatar-body">
 
-        	<p>Let's get started! You'll need an avatar to help other users identify you in JIRA.</p>
+          <p>Let's get started! You'll need an avatar to help other users identify you in JIRA.</p>
            <?php if (isset($result) && $result) { ?>
               <ul class="result">
                   <?php
@@ -58,37 +59,47 @@
               </ul>
           <?php } ?>
 
-			<form enctype="multipart/form-data" id="jira-avatar" action="../controller/uploadController.php" method="post">
+      <form enctype="multipart/form-data" id="jira-avatar" action="../controller/uploadController.php" method="post">
 
-		        <div id="image-holder" class="col-md-3">
-		          <img style="width: 150px;" src="/FinalProject/view/images/add-avatar_2.png" alt="avatar">
-		
-		        </div>
+            <div id="image-holder" class="col-md-3">
+              <?php if (isset($image) && $image) {
+              ?>
+               <img id="avatar" class="img-thumbnail" style="width: 150px;" src="../view/uploaded/<?php echo $image; ?>" alt="avatar">
+              <?php  
+              } else {
 
-				<div class="form-group col-md-9" style="height: 200px;">
+               ?>
+                <img id="avatar" style="width: 150px;" src="/FinalProject/view/images/add-avatar_2.png" alt="avatar">
+              <?php 
+              } 
+
+              ?>
+            </div>
+
+        <div class="form-group col-md-9" style="height: 200px;">
               <input type="hidden" name="MAX_FILE_SIZE" value="<?php if(isset($max)){echo $max;} ?>">
-		        	<label for="image">File input</label>
-		        	<input type="file" id="image" name="image">
-		        	<p class="help-block">Please upload image.</p>
-		   		 </div>
-		   		 
-				<div id="errors"></div>
-				<div class="buttons-container">
-				<div class="row">
-					<div class="col-md-6 text-left">
-						<div class="buttons">	
-							<input type="submit" name="upload" class="btn btn-primary" value="Upload">
-						</div>
-					</div>
-					<div class="col-md-6 text-right">
-						<div class="buttons">
-						<button type="button" onclick="location.href = '../controller/nextController.php';" class="btn btn-primary">Next</button>
-						</div>
-					</div>
-					</div>
-				</div> <!-- class="buttons-container" --> 
+              <label for="image">File input</label>
+              <input type="file" id="image" name="image">
+              <p class="help-block">Please upload image.</p>
+        </div>
+
+        <div id="errors"></div>
+        <div class="buttons-container">
+        <div class="row">
+          <div class="col-md-6 text-left">
+            <div class="buttons"> 
+              <input type="submit" name="upload"  class="btn btn-primary" value="Upload">
+            </div>
+          </div>
+          <div class="col-md-6 text-right">
+            <div class="buttons">
+            <button type="button" onclick="location.href = '../controller/nextController.php';" class="btn btn-primary">Next</button>
+            </div>
+          </div>
+          </div>
+        </div> <!-- class="buttons-container" --> 
         
-			</form>
+      </form>
 
 
         </div> <!-- class="register-body" -->
