66086187c5dd88ba9331af7cdd7d3534265ca6bd
Krasimir <Krasimir>
2017-04-27 02:38:25 +0300

    single project tasks

--- controller/ViewProjectController.php
+++ controller/ViewProjectController.php
@@ -31,11 +31,13 @@
 			$users = ProjectDAO::getProjectAssocUsers($name);
 			
 			$projectTasks = TaskDAO::getProjectTasks($name);
+		
 			
 			$toDoTasks = $projectTasks[0];
 			$workingOnTasks = $projectTasks[1];
 			$doneTasks = $projectTasks[2];
-
+		
+			$colors = array("Low"=>"#53ff1a","Medium"=>"#ffff1a","High"=>"#ffbf00","Escalated"=>"red",);
 		}catch (Exception $e){
 			$message =  $e->getMessage();
 		}
